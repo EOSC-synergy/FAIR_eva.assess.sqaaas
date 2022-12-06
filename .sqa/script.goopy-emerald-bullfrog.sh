ef91490b48adb18c798c535d2c15843d3b07054a
orviz <orviz@ifca.unican.es>
2022-12-06 23:32:54 +0100

    Add JePL file structure

---
+++ .sqa/script.goopy-emerald-bullfrog.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/EOSC-synergy/FAIR_eva &&
+    hadolint */Dockerfile* --failure-threshold error
+)
--- .sqa/script.woozy-brass-turkey.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/EOSC-synergy/FAIR_eva &&
-    licensee detect . --json
-)
